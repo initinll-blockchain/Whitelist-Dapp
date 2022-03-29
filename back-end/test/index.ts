4b7e2b261f1b79a4883adcfca100f30de21273e3
Nitin Londhe <nitin.londhe@genmills.com>
2022-03-30 01:31:15 +0530

    added new contract Whitelist with tests

--- back-end/test/index.ts
+++ back-end/test/index.ts
@@ -1,19 +1,56 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Whitelist", function () {
+  it("Should add new address to whitelist", async function (){
+    const Whitelist = await ethers.getContractFactory("Whitelist");
+    const whitelist = await Whitelist.deploy(10);
+    await whitelist.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const addAddressToWhitelistTx = await whitelist.addAddressToWhitelist();
+    await addAddressToWhitelistTx.wait();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    expect(await whitelist.numOfAddressesWhitelisted()).to.equal(1);
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  it("Should add multiple new addresses to whitelist", async function (){
+    const [owner, randomPerson] = await ethers.getSigners();
+    const Whitelist = await ethers.getContractFactory("Whitelist");
+    const whitelist = await Whitelist.deploy(10);
+    await whitelist.deployed();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const addAddressToWhitelistTx1 = await whitelist.addAddressToWhitelist();
+    await addAddressToWhitelistTx1.wait();
+
+    const addAddressToWhitelistTx2 = await whitelist.connect(randomPerson).addAddressToWhitelist();
+    await addAddressToWhitelistTx2.wait();
+
+    expect(await whitelist.numOfAddressesWhitelisted()).to.equal(2);
+  });
+
+  it("Should not add existing address to whitelist", async function (){
+    const Whitelist = await ethers.getContractFactory("Whitelist");
+    const whitelist = await Whitelist.deploy(10);
+    await whitelist.deployed();
+
+    const addAddressToWhitelistTx1 = await whitelist.addAddressToWhitelist();
+    await addAddressToWhitelistTx1.wait();
+
+    await expect(whitelist.addAddressToWhitelist()).to.be.revertedWith("Sender has already been whitelisted");
+  });
+
+  it("Should not add addresses to whitelist more than max limit", async function (){
+    const [owner, randomPerson1, randomPerson2] = await ethers.getSigners();
+    const Whitelist = await ethers.getContractFactory("Whitelist");
+    const whitelist = await Whitelist.deploy(2);
+    await whitelist.deployed();
+
+    const addAddressToWhitelistTx1 = await whitelist.addAddressToWhitelist();
+    await addAddressToWhitelistTx1.wait();
+
+    const addAddressToWhitelistTx2 = await whitelist.connect(randomPerson1).addAddressToWhitelist();
+    await addAddressToWhitelistTx2.wait();
+
+    await expect(whitelist.connect(randomPerson2).addAddressToWhitelist()).to.be.revertedWith("More addresses cant be added, limit reached");
   });
 });
