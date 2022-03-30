1edfc5a7cd2aa2ee7c6c7f8745b33583b9b52cb7
Nitin Londhe <nitin.londhe@genmills.com>
2022-03-31 01:33:13 +0530

    added app.css and index html

--- front-end/src/routes/index.svelte
+++ front-end/src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '../app.css';
+	import cryptoDevs from '$lib/assets/crypto-devs.svg';
+</script>
+
+<div>
+	<div class="main">
+		<div>
+			<h1 class="title">Welcome to Crypto Devs!</h1>
+			<div class="description">Its an NFT collection for developers in Crypto.</div>
+			<div class="description">
+				{0} have already joined the Whitelist
+			</div>
+			button
+		</div>
+		<div>
+			<img class="image" alt="crypto Devs logo" src={cryptoDevs} />
+		</div>
+	</div>
+	<footer class="footer">Made with &#10084; by Crypto Devs</footer>
+</div>
